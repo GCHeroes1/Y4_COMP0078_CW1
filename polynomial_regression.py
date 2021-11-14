2754a42c7f83fd247a556ff31d7c932cc356a941
kimdanny <dannykim153@gmail.com>
2021-11-14 02:06:00 +0000

    polynomial regression

---
+++ polynomial_regression.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+class PolynomialRegression():
+
+    def __init__(self, degree, learning_rate=0.01, iterations=10000):
+        self.degree = degree
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.X = None
+        self.Y = None
+        self.m = None
+        self.n = None
+        self.W = None
+
+    # function to transform X
+    def transform(self, X):
+        self.m, self.n = X.shape
+        # initialize X_transform
+        X_transform = np.ones((self.m, 1))
+
+        j = 0
+        for j in range(self.degree + 1):
+            if j != 0:
+                x_pow = np.power(X, j)
+                # append x_pow to X_transform 2-D array
+                X_transform = np.append(X_transform, x_pow.reshape(-1, 1), axis=1)
+
+        return X_transform
+
+    # model training
+    def fit(self, X, Y, method='ne'):
+        self.X = X
+        self.Y = Y
+
+        # transform X for polynomial h( x ) = w0 * x^0 + w1 * x^1 + w2 * x^2 + ........+ wn * x^n
+        X_transform = self.transform(self.X)
+
+        if method == 'gd':
+            # gradient descent with MSE cost function
+            self.W = np.zeros(self.degree + 1)
+            for i in range(self.iterations):
+                y_hat = self.predict(self.X)
+                error = y_hat - self.Y
+
+                # update weights by derivative of MSE
+                self.W = self.W - self.learning_rate * (2 / self.m) * np.dot(X_transform.T, error)
+
+        elif method == 'ne':
+            self.W = np.linalg.inv(X_transform.T.dot(X_transform)).dot(X_transform.T).dot(self.Y)
+
+        else:
+            raise NotImplementedError
+
+        return self
+
+    def predict(self, X):
+        # transform X for polynomial h( x ) = w0 * x^0 + w1 * x^1 + w2 * x^2 + ........+ wn * x^n
+        X_transform = self.transform(X)
+        return np.dot(X_transform, self.W)
+
+
+if __name__ == "__main__":
+    X = np.array([[1, 2, 3, 4]])
+    X = X.T
+    Y = np.array([3, 2, 0, 5])
+
+    # model training
+    model = PolynomialRegression(degree=3)
+    X_transform = model.transform(X)
+    print(X_transform)
+
+    model.fit(X, Y)
+    print(model.W)
+
+    # Prediction on training set
+    Y_pred = model.predict(X)
+
+    # Visualization
+    plt.scatter(X, Y, color='blue')
+    plt.plot(X, Y_pred, color='orange')
+    plt.title('X vs Y')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
--- q1.py
+++ q1.py
@@ -11,17 +11,22 @@ x_data = [1, 2, 3, 4]
 y_data = [3, 2, 0, 5]
 sns.set_theme()
 
+
 def constant(x, a, b):
     return 1
+
 
 def linear(x, a, b):
     return a * x + b
 
+
 def quadratic(x, a, b, c):
-    return (a * x) + (b * x**2) + c
+    return (a * x) + (b * x ** 2) + c
+
 
 def cubic(x, a, b, c, d):
-    return (a * x) + (b * x**2) + (c * x**3) + d
+    return (a * x) + (b * x ** 2) + (c * x ** 3) + d
+
 
 def get_y_hats(x_data, objective_function, *args):
     y_hats = []
@@ -30,18 +35,19 @@ def get_y_hats(x_data, objective_function, *args):
     y_hats = np.array(y_hats)
     return y_hats
 
+
 def get_mse(y, y_hat):
     errors = y - y_hat
     squared_errors = np.square(errors)
     mse = sum(squared_errors) / len(squared_errors)
     return mse
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    
     # Fi_1
     sns.scatterplot(x=x_data, y=y_data)
-    pars, _ = curve_fit(f=constant, xdata=x_data, ydata=y_data, bounds=(-np.inf, np.inf))
+    pars, _ = curve_fit(f=constant, xdata=x_data, ydata=y_data)
     sns.lineplot(x_data, constant(x_data, *pars), linestyle='--', linewidth=2, color='black')
 
     mse = get_mse(y_data, np.array([1, 1, 1, 1]))
@@ -74,7 +80,7 @@ if __name__ == '__main__':
     # Fi_4
     popt, _ = curve_fit(cubic, x_data, y_data)
     a, b, c, d = popt
-    print('y = %.5f * x + %.5f * x^2 + %.5f + x^3 + %.5f' % (a, b, c, d)) # THIS IS THE SAME AS IN THE BRIEF :D
+    print('y = %.5f * x + %.5f * x^2 + %.5f + x^3 + %.5f' % (a, b, c, d))  # THIS IS THE SAME AS IN THE BRIEF :D
     x_line = arange(1, 4.5, 0.1)
     y_line = cubic(x_line, a, b, c, d)
     sns.lineplot(x=x_line, y=y_line, linestyle='--', linewidth=2, color='green', legend='auto')
@@ -84,5 +90,5 @@ if __name__ == '__main__':
     print(f"MSE for Fi_4: {mse}")
 
     plt.title("polynomial fitting for each base of dimension")
-    #TODO: lengend with sns...
+    # TODO: lengend with sns...
     plt.savefig('./plots/q1a.png')
