592fa41c82af533ae3c8e538fc18d731c16067a2
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-11-14 19:41:02 +0000

    starting off q5

--- q4.py
+++ q4.py
@@ -3,7 +3,7 @@ from numpy.linalg import inv
 import csv
 import random
 import polynomial_regression as regression
-
+import matplotlib.pyplot as plt
 
 #4a - Naive Regression
 
@@ -51,20 +51,19 @@ if __name__ == '__main__':
 		for row in csvreader:
 			rows.append(row)
 
-	# print(len(rows))
-
 	training_set_size = round(len(rows) * 2/3)
 	testing_set_size = len(rows) - training_set_size
 
 	# print(training_set_size)
 	# print(testing_set_size)
 
-	training_ones = np.ones(training_set_size)
-	training_ones_matrix = np.mat(training_ones).transpose()
-	testing_ones = np.ones(testing_set_size)
-	testing_ones_matrix = np.mat(testing_ones).transpose()
+	# training_ones = np.ones(training_set_size)
+	# training_ones_matrix = np.mat(training_ones).transpose()
+	# testing_ones = np.ones(testing_set_size)
+	# testing_ones_matrix = np.mat(testing_ones).transpose()
 	# print(len(training_ones_matrix))
 
+	# for i in range (0, 19):
 	training_set, testing_set = sample_training(rows, training_set_size)
 	x_training_set = np.array(np.delete(training_set, np.s_[-1:], axis=1)).astype(float)
 	y_training_set = np.array(np.delete(training_set, np.s_[:-1], axis=1)).astype(float)
@@ -73,10 +72,11 @@ if __name__ == '__main__':
 	y_testing_set = np.array(np.delete(testing_set, np.s_[:-1], axis=1)).astype(float)
 
 	x_training_set = x_training_set.T
+	# y_training_set = y_training_set.T
 
 	# model training
 	x_training_set = x_training_set.T
-	model = regression.PolynomialRegression(degree=0)
+	model = regression.PolynomialRegression(degree=12)
 	X_transform = model.transform(x_training_set)
 	# print(X_transform)
 
@@ -85,9 +85,17 @@ if __name__ == '__main__':
 
 	# Prediction on training set
 	Y_pred = model.predict(x_training_set)
-	print(Y_pred)
+	print(Y_pred[-1][0])
 
-	# print(constant_predictor(x_training_set, y_training_set))
+	# print(np.shape(x_training_set[0]))
+	# print(np.shape(np.transpose(y_training_set[0:12][0])))
+	# plt.scatter(x_training_set[1], np.transpose(y_training_set[0:12]), color='blue')
+	# plt.plot(x_training_set[0], Y_pred, color='orange')
+	# plt.title('X vs Y')
+	# plt.xlabel('X')
+	# plt.ylabel('Y')
+	# plt.show()
+	# print(constant_predictor(x_training_set.T, y_training_set))
 	# trying to do linear regression against y=a
 
 
