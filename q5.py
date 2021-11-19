f8eced9368951f161788626f01665e110492db09
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2021-11-19 16:24:15 +0000

    renamed functions, added dataset splitting function

--- q5.py
+++ q5.py
@@ -1,13 +1,16 @@
+import numpy
 import numpy as np
+import q4
 from numpy.linalg import inv
 import csv
 import random
+import math
 import polynomial_regression as regression
 import matplotlib.pyplot as plt
 
 # def argmin(array):
-	# argmin returns the indicies of the minimum element
-	# return np.argmin(array)
+# argmin returns the indicies of the minimum element
+# return np.argmin(array)
 
 # def sum(array):
 # 	return np.sum(array)
@@ -17,20 +20,27 @@ import matplotlib.pyplot as plt
 # 	for i in range (start, end+1):
 # 		sum += data[i]
 # 	return sum
+gamma_values = [2 ** (-40), 2 ** (-39), 2 ** (-38), 2 ** (-37), 2 ** (-36), 2 ** (-35), 2 ** (-34), 2 ** (-33),
+                2 ** (-32), 2 ** (-31), 2 ** (-30), 2 ** (-29), 2 ** (-28), 2 ** (-27), 2 ** (-26)]
+sigma_values = [2 ** 7, 2 ** 7.5, 2 ** 8, 2 ** 8.5, 2 ** 9, 2 ** 9.5, 2 ** 10, 2 ** 10.5, 2 ** 11, 2 ** 11.5, 2 ** 12,
+                2 ** 12.5, 2 ** 13]
+
 
 def nested_summation_alphastar(l, alpha, x, y):
 	sum = 0
 	inner_sum = 0
-	for i in range (l+1):
-		for j in range (l+1):
-			inner_sum+= alpha[j-1] * kernelK(x, i, j) - y[i]
+	for i in range(l + 1):
+		for j in range(l + 1):
+			inner_sum += alpha[j - 1] * kernelK(x, i, j) - y[i]
 		sum += inner_sum ** 2
 		inner_sum = 0
 	return sum
 
+
 def kernelK(x, i, j):
-	#request for k_ij is the inner product of x_i and x_j
-	return np.inner(x[i-1], x[j-1])
+	# request for k_ij is the inner product of x_i and x_j
+	return np.inner(x[i - 1], x[j - 1])
+
 
 def kernelKConstruction(x):
 	size = len(x)
@@ -40,6 +50,7 @@ def kernelKConstruction(x):
 			kernel.append(kernelK(x, i, j))
 	return np.reshape(kernel, (size, size))
 
+
 def equation_10(n, data, weight, predictions, gamma):
 	# w* = argmin _ (for all w in real) * 1/l *
 	sum = 0
@@ -47,26 +58,48 @@ def equation_10(n, data, weight, predictions, gamma):
 		math = data.T[i] * weight - predictions[i]
 		sum += math ** 2
 	part_2 = gamma * np.matmul(weight.T, weight)
-	return np.argmin(1/n * sum + part_2)
-	# return np.argmin(1/n * np.sum(np.matmul(data.T, weight) - predictions)**2 + lamb * np.matmul(weight.T, weight))
+	return np.argmin(1 / n * sum + part_2)
 
-def equation_11(n, gamma, alpha, x, y):
-	sum_1 = np.argmin(1/n * nested_summation_alphastar(n, alpha, x, y))
+
+# return np.argmin(1/n * np.sum(np.matmul(data.T, weight) - predictions)**2 + lamb * np.matmul(weight.T, weight))
+
+def equation_11(n, gamma, alpha, x, y): # alpha star dual optimisation
+	sum_1 = np.argmin(1 / n * nested_summation_alphastar(n, alpha, x, y))
 	sum_2 = gamma * np.matmul(np.matmul(alpha.T, kernelKConstruction(x)), alpha)
 	return sum_1 + sum_2
 
-def equation_12(x, gamma, n, y):
+
+def calc_alpha_star(x, gamma, n, y): #alpha star, solved dual
 	kernel = kernelKConstruction(x)
 	identity = np.identity(n)
 	part_1 = (kernel + gamma * n * identity).inv
-	return np.matmul(part_1, y)
+	return part_1.dot(y)
+	# return np.matmul(part_1, y)
 
-def equation_13(x, y, gamma, test, n):
-	sum = 0
+
+def calc_y_test(x, y, gamma, test, n):  # generating y_test
+	y_test = 0
 	for i in range(n):
-		math = equation_12(x, gamma, n, y)[i] * kernelK(x, i, test)
-		sum += math
-	return sum
+		math = calc_alpha_star(x, gamma, n, y)[i] * kernelK(x, i, test)
+		y_test += math
+	return y_test
+
+
+def gaussian_kernel(x, i, j, sigma): #eq 14, the gaussian kernel
+	numerator = numpy.abs(x[i] - x[j]) ** 2
+	denominator = 2 * (sigma ** 2)
+	return np.exp(-numerator / denominator)
+
+def five_fold_dataset(data):
+	random.shuffle(data)
+	split = math.floor(len(data)/5)
+	one, two, three, four, five = [], [], [], [], []
+	one.append(data[0: split])
+	two.append(data[split:2*split])
+	three.append(data[2*split:3*split])
+	four.append(data[3*split:4*split])
+	five.append(data[4*split:5*split])
+	return one, two, three, four, five
 
 if __name__ == '__main__':
 	data = np.arange(9, 18).reshape((3, 3))
@@ -74,8 +107,8 @@ if __name__ == '__main__':
 	predictions = np.arange(6, 9).reshape((3, 1))
 	print(data)
 	print(data[1][1])
-	# lamb = 5
-	# print(data)
-	# print(weight)
-	# print(predictions)
-	# print(equation_10(3, data, weight, predictions, lamb))
+# lamb = 5
+# print(data)
+# print(weight)
+# print(predictions)
+# print(equation_10(3, data, weight, predictions, lamb))
