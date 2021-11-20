c22d0d25436b837c13a11af753eee5feee484651
kimdanny <dannykim153@gmail.com>
2021-11-20 03:09:23 +0000

    q2b

--- q2.py
+++ q2.py
@@ -4,6 +4,7 @@ from numpy import arange
 import seaborn as sns
 from polynomial_regression import PolynomialRegression
 from sys import exit
+from q1 import get_mse
 
 std = 0.07
 
@@ -14,102 +15,6 @@ def function(x, epsilon):
 
 def random_sample(n_times):
 	return np.linspace(start=0, stop=1, num=n_times)
-
-
-def degree_1(x, a, b):
-	return a
-
-
-def degree_2(x, a, b):
-	return (a * x) + b
-
-
-def degree_3(x, a, b, c):
-	return (a * x) + (b * x ** 2) + c
-
-
-def degree_4(x, a, b, c, d):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + d
-
-
-def degree_5(x, a, b, c, d, e):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + e
-
-
-def degree_6(x, a, b, c, d, e, f):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + f
-
-
-def degree_7(x, a, b, c, d, e, f, g):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + g
-
-
-def degree_8(x, a, b, c, d, e, f, g, h):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + h
-
-
-def degree_9(x, a, b, c, d, e, f, g, h, i):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + i
-
-
-def degree_10(x, a, b, c, d, e, f, g, h, i, j):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + j
-
-
-def degree_11(x, a, b, c, d, e, f, g, h, i, j, k):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + k
-
-
-def degree_12(x, a, b, c, d, e, f, g, h, i, j, k, l):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + l
-
-
-def degree_13(x, a, b, c, d, e, f, g, h, i, j, k, l, m):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + (l * x ** 12) + m
-
-
-def degree_14(x, a, b, c, d, e, f, g, h, i, j, k, l, m, n):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + (l * x ** 12) + (m * x ** 13) + n
-
-
-def degree_15(x, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + (l * x ** 12) + (m * x ** 13) + (n * x ** 14) + o
-
-
-def degree_16(x, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + (l * x ** 12) + (m * x ** 13) + (n * x ** 14) + (
-				   o * x ** 15) \
-		   + p
-
-
-def degree_17(x, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q):
-	return (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + (
-			h * x ** 8) \
-		   + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + (l * x ** 12) + (m * x ** 13) + (n * x ** 14) + (
-				   o * x ** 15) \
-		   + (p * x ** 16) + q
-
-
-def degree_18(x, r, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q):
-	return r + (a * x) + (b * x ** 2) + (c * x ** 3) + (d * x ** 4) + (e * x ** 5) + (f * x ** 6) + (g * x ** 7) + \
-		   (h * x ** 8) + (i * x ** 9) + (j * x ** 10) + (k * x ** 11) + (l * x ** 12) + (m * x ** 13) + \
-		   (n * x ** 14) + (o * x ** 15) + (p * x ** 16) + (q * x ** 17)
 
 
 def MSE(x_data, y_data, parameters, function):
@@ -153,58 +58,62 @@ if __name__ == '__main__':
 	X = np.array([x_data]).T
 	Y = np.array(y_data)
 
-	model_fi_2 = PolynomialRegression(degree=1)
-	model_fi_5 = PolynomialRegression(degree=4)
-	model_fi_10 = PolynomialRegression(degree=9)
-	model_fi_14 = PolynomialRegression(degree=13)
-	model_fi_18 = PolynomialRegression(degree=17, normalize=True)
-
-	model_fi_2.fit(X, Y)
-	y_hat_2 = model_fi_2.predict(X)
-	model_fi_5.fit(X, Y)
-	y_hat_5 = model_fi_5.predict(X)
-	model_fi_10.fit(X, Y)
-	y_hat_10 = model_fi_10.predict(X)
-	model_fi_14.fit(X, Y)
-	y_hat_14 = model_fi_14.predict(X)
-	model_fi_18.fit(X, Y)
-	y_hat_18 = model_fi_18.predict(X)
-
-	# What K=18 should look like
-	from sklearn.preprocessing import PolynomialFeatures
-	from sklearn.linear_model import LinearRegression
-	poly_feat = PolynomialFeatures(degree=17)
-	poly_x = poly_feat.fit_transform(X)
-	linear_model = LinearRegression()
-	linear_model.fit(poly_x, Y)
-	y_hat_18 = linear_model.predict(poly_x)
+	color_map = {
+		2: 'green', 5: 'orange', 10: 'black', 14: 'red', 18: 'blue'
+	}
 
 
-	plt.plot(x_data, y_hat_2, label="k=2", color='black')
-	plt.plot(x_data, y_hat_5, label="k=5", color='green')
-	plt.plot(x_data, y_hat_10, label="k=10", color='orange')
-	plt.plot(x_data, y_hat_14, label="k=14", color='blue')
-	plt.plot(x_data, y_hat_18, label="k=18", color='red')
+	def fit_polynomials_by_bases(base_list: list, get_only_errors=False) -> list:
+		"""
+		Train polynomialRegression, predict based on trained data, get training error
+		:param base_list: list of K's (bases)
+		:param get_only_errors: if True, does not plot a graph
+		:return: training errors (mse) of each bases in list
+		"""
+		train_error = []
+		for k in base_list:
+			model_fi_k = PolynomialRegression(degree=k-1)
+			model_fi_k.fit(X, Y)
+			y_hat_k = model_fi_k.predict(X)
+
+			if not get_only_errors:
+				plt.plot(x_data, y_hat_k, label=f'k={k}', color=color_map[k])
+
+			# Calculate MSE as a training error
+			mse = get_mse(Y, y_hat_k)
+			train_error.append(mse)
+
+		return train_error
+
+	_ = fit_polynomials_by_bases([2, 5, 10, 14, 18])
 
 	plt.legend()
 	plt.title("Q2aii: sin graph with different bases")
 	plt.savefig('./plots/q2aii.png')
 
-	exit(1)
+	###############
+	# Question 2b #
+	###############
 
-	print(MSE)
-	MSE_x_Data = arange(0, 18, 1)
+	# Plot in Normal scale
 	plot3 = plt.figure(3)
-	ln_MSE = np.log(MSE)
-	sns.lineplot(x=MSE_x_Data, y=ln_MSE, linewidth=2, color='black')
-	# plt.plot(MSE)
-	# plt.yscale("log")
-	# print(MSE_2)
-	# print(MSE_5)
-	# print(MSE_10)
-	# print(MSE_14)
-	# print(MSE_18)
 
-	sample_t = random_sample(1000)
+	bases = [x+1 for x in range(18)]
+	training_error = fit_polynomials_by_bases(bases, get_only_errors=True)
+	plt.plot(bases, training_error)
+	plt.title("Q2b: train error Vs. polynomial dimension")
+	plt.savefig('./plots/q2b_no_log.png')
 
-	plt.show()
+	# Plot in log scale
+	plot4 = plt.figure(4)
+
+	bases = [x + 1 for x in range(18)]
+	training_error = fit_polynomials_by_bases(bases, get_only_errors=True)
+	plt.plot(bases, training_error)
+	plt.title("Q2b: train error Vs. polynomial dimension (log scale)")
+	plt.yscale('log')
+	plt.savefig('./plots/q2b_log.png')
+
+	###############
+	# Question 2c #
+	###############
